07ed0fc320dde16288cbba9e0ee10a7accdf34d0
namu1 <cyh0214@namuplanet.com>
2022-11-17 14:30:16 +0900

    Add pylint (google style) and Update all py files accoring to google pylint

--- channel/app/main.py
+++ channel/app/main.py
@@ -1,7 +1,19 @@
+"""
+    channel api
+"""
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/stock/{id}")
-def read_stock(id: int):
-    return {"id": id, "name": f"stock_{id}"}
+@app.get("/read/item/{prod_id}")
+def read_item(prod_id: int) -> dict:
+  """read_item
+
+  Args:
+      prod_id (int): 상품아이디
+
+  Returns:
+      json: 상품정보
+  """
+
+  return {"id": prod_id, "name": f"stock_{prod_id}"}
--- product/app/main.py
+++ product/app/main.py
@@ -1,4 +1,5 @@
 """
+
     Product-server
 """
 from fastapi import FastAPI, HTTPException
@@ -6,22 +7,27 @@ from fastapi import FastAPI, HTTPException
 app = FastAPI()
 
 fake_db = [
-    {"id": 1, "name": "신발", "price": 29700, "category": "shoes"},
-    {"id": 2, "name": "좋은 신발", "price": 189700, "category": "shoes"},
-    {"id": 3, "name": "평범한 신발", "price": 56000, "category": "shoes"},
-    {"id": 4, "name": "가디건", "price": 206000, "category": "outer"},
-    {"id": 5, "name": "가죽자켓", "price": 346500, "category": "outer"},
+  {"id": 1, "name": "신발", "price": 29700, "category": "shoes"},
+  {"id": 2, "name": "좋은 신발", "price": 189700, "category": "shoes"},
+  {"id": 3, "name": "평범한 신발", "price": 56000, "category": "shoes"},
+  {"id": 4, "name": "가디건", "price": 206000, "category": "outer"},
+  {"id": 5, "name": "가죽자켓", "price": 346500, "category": "outer"},
 ]
 
+
 @app.get("/product/category/{category_id}")
-def read_items_by_category(category_id: str):
-    items = list(filter(lambda x: x["category"] == category_id, fake_db))
-    return items
+def read_items_by_category(category_id: str) -> dict:
 
-@app.get("/product/id/{id}")
-def read_item(id: int):
-    item = [x for x in fake_db if x["id"] == id]
-    if len(item) == 0:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return item[0]
+  items = list(filter(lambda x: x["category"] == category_id, fake_db))
 
+  return items
+
+
+@app.get("/product/id/{prod_id}")
+def read_item(prod_id: int) -> dict:
+
+  item = [x for x in fake_db if x["id"] == prod_id]
+  if len(item) == 0:
+    raise HTTPException(status_code=404, detail="Item not found")
+
+  return item[0]
